4532bbc47d1467c97aea21399f3838dccc16f852
Kurt <kurt@timetreeapp.com>
2022-08-05 15:08:55 +0900

    fix: 🐛 remove sample code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,2 @@
 import './style.scss'
 import './index.scss'
-
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
